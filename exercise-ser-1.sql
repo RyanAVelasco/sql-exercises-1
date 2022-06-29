8ddb7d75ac1d3383a3d7a6fb72085cedc97a75d3
RyanAVelasco <ryanv0110@gmail.com>
2022-06-28 21:12:40 -0500

    small update

---
+++ exercise-ser-1.sql
@@ -0,0 +1,13 @@
+/* 
+MySQL exercises from W3 Resouce
+MySQL Exercises, Practice, Solution:
+https://w3resource.com/mysql-exercises/
+*/
+
+/*
+Exercises on Data Manipulation Language (DML) & 
+Data Definition Language (DDL)
+MySQL Create Table statement [20 Exercises] 
+*/
+
+CREATE TABLE
